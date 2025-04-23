52a5df5d848d003689c9dd2e23281ca3b3b3177d
Yohan <yohanlahiru86@gmail.com>
2025-04-24 01:58:44 +0530

    retry with hg-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'./',
+  base:'https://yohan86.github.io/slnews/',
   plugins: [
     react(),
     tailwindcss(),
