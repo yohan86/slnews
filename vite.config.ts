1060c9ac67ae9172a96fab7f97e2ffe344ff6080
Yohan <yohanlahiru86@gmail.com>
2025-04-24 02:28:23 +0530

    removed favicon and retry with base field

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'https://yohan86.github.io/slnews/dist/',
+  base:'/slnews/',
   plugins: [
     react(),
     tailwindcss(),
