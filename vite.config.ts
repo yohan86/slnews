094bf739302a1e0ad720a962622315fb7a66ab1d
Yohan <yohanlahiru86@gmail.com>
2025-04-24 02:09:56 +0530

    retry with base field

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'https://yohan86.github.io/slnews/',
+  base:'https://yohan86.github.io/slnews/dist/',
   plugins: [
     react(),
     tailwindcss(),
