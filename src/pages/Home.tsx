4d8a500e8e3dc73c4675af4b7906e28c9824c3db
Yohan <yohanlahiru86@gmail.com>
2025-04-24 22:17:28 +0530

    added test text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,7 @@ const Home = () => {
           
     <CategorySlider category="4z0e0xwcLp8C3QGc7vrtAI" />
     <div className="main-wrapper w-[95%] md:w-[98%] m-auto p-2">
+      home
       <PostsByCategoryList />
     </div>
     </>
