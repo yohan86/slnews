d2266c33ca9464d0765291cac733fce9ee122ada
Yohan <yohanlahiru86@gmail.com>
2025-04-27 23:32:38 +0530

    added script and chnage base field

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/slnews/",
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: "/slnews/",
 })
