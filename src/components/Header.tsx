560b84f3e652b7b82489d0430d23748533ef7e23
Yohan <yohanlahiru86@gmail.com>
2025-04-28 23:06:55 +0530

    fixed link issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="header-wrapper  w-full">
         <div className="header flex justify-between w-full  max-w-[90%] m-auto p-2">
             <div className="site-logo">
-                <a href="/"><img src={logo} alt="Site Logo" className="h-[55px] md:h-[100px] lg:h-[110px]" /></a>
+            <a  href={import.meta.env.BASE_URL} title="Read More"><img src={logo} alt="Site Logo" className="h-[55px] md:h-[100px] lg:h-[110px]" /></a>
             </div>
             <div className="h-add-banner w-[200px] md:w-[500px] lg:w-[570px] h-[55px] md:h-[100px] bg-green-800 float-rights"></div>
         </div>
